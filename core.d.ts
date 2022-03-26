ac7a71d19515a1540fd08b2c20bee252fcb42ea0
Martin Goulet <martin.goulet@live.ca>
2022-03-26 05:46:54 -0400

    Add 3 methods

--- core.d.ts
+++ core.d.ts
@@ -1,9 +1,7 @@
 declare const g_gamethemeurl: string;
 
 declare namespace ebg {
-
     namespace core {
-
         interface gamegui {
             /////////////////////////////////////////////////
             // General tips
@@ -35,7 +33,7 @@ declare namespace ebg {
              */
             getActivePlayers: () => string[];
             /**
-             * Return true if the game is in realtime. 
+             * Return true if the game is in realtime.
              * Note that having a distinct behavior in realtime and turn-based should be exceptional.
              */
             bRealtime: boolean;
@@ -53,22 +51,27 @@ declare namespace ebg {
             // Animations
 
             /**
-             * Sliding element on the game area is the recommended and the most used way to animate your game interface. 
+             * Sliding element on the game area is the recommended and the most used way to animate your game interface.
              * Using slides allow players to figure out what is happening on the game, as if they were playing with the real boardgame.
              * @param mobile_obj ID of the object to move. This object must be "relative" or "absolute" positioned.
              * @param target_obj ID of the target object. This object must be "relative" or "absolute" positioned.
              * @param duration Duration in millisecond of the slide. The default is 500 milliseconds.
              * @param delay If you defines a delay, the slide will start only after this delay
-             * @note 
+             * @note
              * it is not mandatory that mobile_obj and target_obj have the same size. If their size are different, the system slides the center of mobile_obj to the center of target_obj.
              * @note
              * The method returns an dojo.fx animation, so you can combine it with other animation if you want to. It means that you have to call the "play()" method, otherwise the animation WON'T START.
              * @example
              * this.slideToObject( "some_token", "some_place_on_board" ).play();
              */
-            slideToObject: (mobile_obj: string, target_obj: string, duration?: number, delay?: number) => DojoFxAnimation;
+            slideToObject: (
+                mobile_obj: string,
+                target_obj: string,
+                duration?: number,
+                delay?: number
+            ) => DojoFxAnimation;
             /**
-             * This method does exactly the same as "slideToObject", except than you can specify some (x,y) coordinates. 
+             * This method does exactly the same as "slideToObject", except than you can specify some (x,y) coordinates.
              * This way, "mobile_obj" will slide to the specified x,y position relatively to "target_obj".
              * @param mobile_obj ID of the object to move. This object must be "relative" or "absolute" positioned.
              * @param target_obj ID of the target object. This object must be "relative" or "absolute" positioned.
@@ -76,44 +79,63 @@ declare namespace ebg {
              * @param target_y Vertical position in pixel on target_obj
              * @param duration Duration in millisecond of the slide. The default is 500 milliseconds.
              * @param delay If you defines a delay, the slide will start only after this delay
-             * @note 
+             * @note
              * it is not mandatory that mobile_obj and target_obj have the same size. If their size are different, the system slides the center of mobile_obj to the center of target_obj.
              * @note
              * The method returns an dojo.fx animation, so you can combine it with other animation if you want to. It means that you have to call the "play()" method, otherwise the animation WON'T START.
              * @example
              * this.slideToObjectPos( "some_token", "some_place_on_board", 0, 10 ).play();
              */
-            slideToObjectPos: (mobile_obj: string, target_obj: string, target_x: number, target_y: number, duration?: number, delay?: number) => DojoFxAnimation;
+            slideToObjectPos: (
+                mobile_obj: string,
+                target_obj: string,
+                target_x: number,
+                target_y: number,
+                duration?: number,
+                delay?: number
+            ) => DojoFxAnimation;
             /**
-             * This method is useful when you want to slide a temporary HTML object from one place to another. 
-             * As this object does not exists before the animation and won't remain after, it could be complex to 
-             * create this object (with dojo.place), to place it at its origin (with placeOnObject) to slide it (with slideToObject) 
+             * This method is useful when you want to slide a temporary HTML object from one place to another.
+             * As this object does not exists before the animation and won't remain after, it could be complex to
+             * create this object (with dojo.place), to place it at its origin (with placeOnObject) to slide it (with slideToObject)
              * and to make it disappear at the end.
              * @param mobile_obj_html Piece of HTML code that represent the object to slide
-             * @param mobile_obj_parent ID of an HTML element of your interface that will be the parent of this temporary HTML object. 
+             * @param mobile_obj_parent ID of an HTML element of your interface that will be the parent of this temporary HTML object.
              * @param from ID of the origin of the slide.
              * @param to ID of the target of the slide.
              * @param duration Duration in millisecond of the slide. The default is 500 milliseconds.
              * @param delay If you defines a delay, the slide will start only after this delay
-             * 
+             *
              * @example
              * this.slideTemporaryObject( '<div class="token_icon"></div>', 'tokens', 'my_origin_div', 'my_target_div' ).play();
              */
-            slideTemporaryObject: (mobile_obj_html: string, mobile_obj_parent: string, from: string, to: string, duration?: number, delay?: number) => DojoFxAnimation;
+            slideTemporaryObject: (
+                mobile_obj_html: string,
+                mobile_obj_parent: string,
+                from: string,
+                to: string,
+                duration?: number,
+                delay?: number
+            ) => DojoFxAnimation;
 
             /**
              * This method is a handy shortcut to slide an existing HTML object to some place then destroy it upon arrival. It can be used for example to move a victory token or a card from the board to the player panel to show that the player earns it, then destroy it when we don't need to keep it visible on the player panel.
-             * @param node 
-             * @param to 
+             * @param node
+             * @param to
              * @param duration Duration in millisecond of the slide. The default is 500 milliseconds.
              * @param delay If you defines a delay, the slide will start only after this delay
-             * 
-             * @note 
+             *
+             * @note
              * It works the same as this.slideToObject and takes the same arguments, but it starts the animation.
              * @example
              * this.slideToObjectAndDestroy( "some_token", "some_place_on_board", 1000, 0 );
              */
-            slideToObjectAndDestroy: (node: string, to: string, duration?: number, delay?: number) => DojoFxAnimation;
+            slideToObjectAndDestroy: (
+                node: string,
+                to: string,
+                duration?: number,
+                delay?: number
+            ) => DojoFxAnimation;
 
             ///////////////////////////////
             // Moving Elements
@@ -126,18 +148,22 @@ declare namespace ebg {
              * @note
              * This is not really an animation, but placeOnObject is frequently used before starting an animation.
              */
-            placeOnObject: (mobile_obj: string, target_obj: string) => void
+            placeOnObject: (mobile_obj: string, target_obj: string) => void;
             /**
              * This method works exactly like placeOnObject, except than you can specify some (x,y) coordinates.
              */
-            placeOnObjectPos: (mobile_obj: string, target_obj: string, target_x: number, target_y: number) => void;
+            placeOnObjectPos: (
+                mobile_obj: string,
+                target_obj: string,
+                target_x: number,
+                target_y: number
+            ) => void;
             /**
              * With this method, you change the HTML parent of "mobile_obj" element. "target_obj" is the new parent of this element. The beauty of attachToNewParent is that the mobile_obj element DOES NOT MOVE during this process.
              * @note
              * What happens is that the method calculate a relative position of mobile_obj to make sure it does not move after the HTML parent changes.
              */
-            attachToNewParent: (mobile_obj: string, target_obj: string) => void
-
+            attachToNewParent: (mobile_obj: string, target_obj: string) => void;
 
             prefs: { [pref_id: number]: BgaGamePreference };
 
@@ -147,32 +173,65 @@ declare namespace ebg {
             /**
              * Add an HTML tooltip to the DOM node (for more elaborate content such as presenting a bigger version of a card).
              */
-            addTooltipHtml: (nodeId: string, html: string, delay?: number) => void;
+            addTooltipHtml: (
+                nodeId: string,
+                html: string,
+                delay?: number
+            ) => void;
             /**
              * Add a simple text tooltip to all the DOM nodes set with this cssClass.
-             * @param cssClass 
+             * @param cssClass
              * @param helpString Specify to display some information about "what is this game element?"
              * @param actionString Specify to display some information about "what happens when I click on this element?".
-             * @param delay 
+             * @param delay
              * @note
              * All concerned nodes must have IDs to get tooltips.
              * @note
              * helpString and actionString : Usually, _() must be used for the text to be marked for translation.
              */
-            addTooltipToClass: (cssClass, helpString: string, actionString: string, delay?: number) => void;
-            addActionButton: (id: string, label: string, method: string | Function, destination?: string, blinking?: boolean, color?: BgaButtonColor) => void;
-            connect: (element: HTMLElement, event: string, handler: string | Function) => any;
+            addTooltipToClass: (
+                cssClass,
+                helpString: string,
+                actionString: string,
+                delay?: number
+            ) => void;
+            addActionButton: (
+                id: string,
+                label: string,
+                method: string | Function,
+                destination?: string,
+                blinking?: boolean,
+                color?: BgaButtonColor
+            ) => void;
+            connect: (
+                element: HTMLElement,
+                event: string,
+                handler: string | Function
+            ) => any;
 
-            connectClass: (cssClassName: string, event: string, handle: string | Function) => void;
+            connectClass: (
+                cssClassName: string,
+                event: string,
+                handle: string | Function
+            ) => void;
             disconnect: (element: HTMLElement, event: string) => void;
             disconnectAll: () => void;
 
             restoreServerGameState: () => void;
-            updatePageTitle() : void;
+            updatePageTitle(): void;
 
-            checkPossibleActions: (action: string, nomessage?: string) => boolean;
+            checkPossibleActions: (
+                action: string,
+                nomessage?: string
+            ) => boolean;
             checkAction: (action: string, nomessage?: boolean) => boolean;
-            ajaxcall: (url: string, parameters: any, obj_callback: any, callback: Function, callback_error: Function) => void;
+            ajaxcall: (
+                url: string,
+                parameters: any,
+                obj_callback: any,
+                callback: Function,
+                callback_error: Function
+            ) => void;
 
             setClientState: (state_name: string, args: any) => void;
 
@@ -182,20 +241,37 @@ declare namespace ebg {
 
             inherited: (args: any) => any;
 
-            confirmationDialog( message: string, yesHandler?: string | Function, noHandler?: string | Function ) : void;
+            confirmationDialog(
+                message: string,
+                yesHandler?: string | Function,
+                noHandler?: string | Function
+            ): void;
+
+            dontPreloadImage(image_file_name: string): void;
+            ensureSpecificGameImageLoading(image_file_names_array: string[]);
+            displayScoring(
+                anchor_id: string,
+                color: string,
+                score: number,
+                duration: number,
+                offset_x?: number,
+                offset_y?: number
+            ): void;
         }
 
         /**
          * To allow Typescript to extends the interface without implementing it
          */
         class gamegui implements gamegui {}
-
     }
-
 }
 
 declare interface INotificationQueue {
     setSynchronous: (event: string, duration: number) => void;
+    setIgnoreNotificationCheck: (
+        event: string,
+        condition: (event: any) => boolean
+    ) => void;
 }
 
 declare interface INotification<TArgs> {
